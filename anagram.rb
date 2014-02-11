7b04ab19637ed47355e8023f021d559794521a4a
Magdalena Lipinska <mag.lipinska@gmail.com>
2014-02-11 16:18:37 -0500

    problem solved

--- anagram.rb
+++ anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+
+class Anagram
+  def initialize(string)
+    @string = string
+  end
+
+  def match(text)
+    text.select do |word|
+      @string.split(//).sort.join() == word.split(//).sort.join()
+    end
+  end
+end
